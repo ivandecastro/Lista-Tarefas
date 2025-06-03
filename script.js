a9fcd8e72876d7cf6c999beede1dde23476753ef
Ivan de Castro Evangelista <ivandecastroevangelista@gmail.com>
2025-06-03 18:40:21 -0300

    Retirando logs desnecessários e adicionando comentários.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const salvarTarefasNoStorage = () => localStorage.setItem('tarefas', JSON.string
 
 //#region Utilitários
 const dataAtual = () => { //Formata a data atual para o padrão "dd/mm/aaaa - hh:mm:ss".
-    const agora = new Date(); //Cria um novo objeto Date com a data e hora atuais
+    const agora = new Date(); //Cria um novo objeto Date com a data e hora atuais.
     return agora.toLocaleDateString() + ' - ' + agora.toLocaleTimeString();
 };
 
@@ -23,26 +23,28 @@ const exibirTodasAsTarefas = () => { //Itera sobre o array de tarefas e exibe ca
 }
 
 const informacoes = () => { //Coleta as informações dos elementos HTML necessários para adicionar uma tarefa.
-    const inputTarefa = document.getElementById('task-input'); //Coleta o input de tarefa
-    const prioridadeValue = document.getElementById('priority-input').value; //Coleta o valor da prioridade selecionada
-    const tasksList = document.getElementById("task-list"); //Coleta a lista de tarefas onde as novas tarefas serão exibidas
-    return [inputTarefa, prioridadeValue, tasksList];
+    const inputTarefa = document.getElementById('task-input'); //Coleta o input de tarefa.
+    const prioridadeValue = document.getElementById('priority-input').value; //Coleta o valor da prioridade selecionada.
+    const tasksList = document.getElementById("task-list"); //Coleta a lista de tarefas onde as novas tarefas serão exibidas.
+    return [inputTarefa, prioridadeValue, tasksList]; //Retorna os valores.
 };
 
-const eventMarcaDagua = () => {
-    document.getElementById('estiloMarcaDagua').addEventListener('change', (e) => {
-        const novoEstilo = e.target.value;
-        localStorage.setItem('estiloMarcaDagua', novoEstilo);
+const eventMarcaDagua = () => { // Permite que a marca d'água seja selecionada para todos os itens em conjunto.
+    //Evento para a troca do estilo da marca d'água.
+    document.getElementById('estiloMarcaDagua').addEventListener('change', (e) => { 
 
-        const todasAsTarefas = document.querySelectorAll('#task-list li');
-        todasAsTarefas.forEach(tarefa => {
-            tarefa.classList.remove('marca-canto', 'marca-centro');
-            if (novoEstilo === 'canto') {
-                tarefa.classList.add('marca-canto');
-            } else if (novoEstilo === 'centro') {
-                tarefa.classList.add('marca-centro');
+        const novoEstilo = e.target.value; //Pega qual será o novo estilo selecionado.
+        localStorage.setItem('estiloMarcaDagua', novoEstilo); //Salva o estilo no localStorage.
+
+        const todasAsTarefas = document.querySelectorAll('#task-list li'); //Pega cada elemento da lista.
+        todasAsTarefas.forEach(tarefa => { //Identifica qual a nova marca d'água.
+            tarefa.classList.remove('marca-canto', 'marca-centro'); //Remove o tipo da marca d'água.
+            if (novoEstilo === 'canto') { //Verifica o tipo da marca d'água (caso seja "canto").
+                tarefa.classList.add('marca-canto'); //Adiciona o estilo "canto" na marca d'água.
+            } else if (novoEstilo === 'centro') { //Verifica o tipo da marca d'água (caso seja "centralizada").
+                tarefa.classList.add('marca-centro'); //Adiciona o estilo "centralizado" na marca d'água.
             }
-            // se for 'nenhum', nada será adicionado
+            //Se for 'nenhum', nada será adicionado.
         });
     });
 };
@@ -50,9 +52,9 @@ eventMarcaDagua();
 
 //Adiciona um evento de tecla "Enter" para o input de tarefa, permitindo adicionar a tarefa ao pressionar "Enter".
 const inputEnter = () => document.getElementById('task-input').addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') { //Verifica se a tecla pressionada é "Enter"
-        adicionar(); //Chama a função para adicionar a tarefa
-        event.preventDefault(); //Previne o comportamento padrão do evento
+    if (event.key === 'Enter') { //Verifica se a tecla pressionada é "Enter".
+        adicionar(); //Chama a função para adicionar a tarefa.
+        event.preventDefault(); //Previne o comportamento padrão do evento.
     }
 });
 inputEnter();
@@ -61,40 +63,39 @@ inputEnter();
 //#region Botões de Ação
 
 const botaoRemover = (indice) => { //Cria um botão para remover uma tarefa específica do array de tarefas.
-    const link = document.createElement('button'); //Cria um elemento de botão
-    link.textContent = "REMOVER"; //Define o texto do botão
-    link.className = "btn-remove";
+    const link = document.createElement('button'); //Cria um elemento de botão.
+    link.textContent = "REMOVER"; //Define o texto do botão.
+    link.className = "btn-remove"; //Adiciona uma classe para o botão.
 
     const [, , listaTarefas] = informacoes(); //Coleta a lista de tarefas para poder remover o item da lista HTML.
 
-    link.addEventListener("click", (event) => { //Adiciona um evento de clique ao botão
-        event.preventDefault(); //Previne o comportamento padrão do botão
+    link.addEventListener("click", (event) => { //Adiciona um evento de clique ao botão.
+        event.preventDefault(); //Previne o comportamento padrão do botão.
 
-        tarefas.splice(indice, 1); //Remove a tarefa do array (usando o índice correto via texto ou alguma outra marca)
-        salvarTarefasNoStorage(); //Salva as alterações no localStorage
+        tarefas.splice(indice, 1); //Remove a tarefa do array (usando o índice correto via texto ou alguma outra marca).
+        salvarTarefasNoStorage(); //Salva as alterações no localStorage.
 
-        listaTarefas.innerHTML = ''; //Limpa a lista de tarefas visualmente
-        exibirTodasAsTarefas()
-        console.log(tarefas);
+        listaTarefas.innerHTML = ''; //Limpa a lista de tarefas visualmente.
+        exibirTodasAsTarefas() //Mostra as tarefas, agora atualizadas.
+        console.log(tarefas); //Log das tarefas. 
     });
 
     return link;
 };
 
 const botaoConfirmar = (indice) => { //Cria um botão para confirmar a conclusão de uma tarefa específica.
-    const link = document.createElement('button'); //Cria um elemento de botão
+    const link = document.createElement('button'); //Cria um elemento de botão.
 
-    let permitido = tarefas[indice].concluida; //Define o status inicial com base na tarefa armazenada
-    aplicarEstiloBotaoConfirmar(link, permitido); //Aplica os estilos ao botão de confirmar
+    let permitido = tarefas[indice].concluida; //Define o status inicial com base na tarefa armazenada.
+    aplicarEstiloBotaoConfirmar(link, permitido); //Aplica os estilos ao botão de confirmar.
 
-    link.addEventListener("click", () => { //Adiciona um evento de clique ao botão
-        permitido = !permitido; //Inverte o estado de conclusão da tarefa
+    link.addEventListener("click", () => { //Adiciona um evento de clique ao botão.
+        permitido = !permitido; //Inverte o estado de conclusão da tarefa.
 
 
-        tarefas[indice].concluida = permitido; //Atualiza o objeto no array com base no novo estado
-        salvarTarefasNoStorage(); //Salva as alterações no localStorage
-        aplicarEstiloBotaoConfirmar(link, permitido, 'COMPLETA', 'PENDENTE'); //Aplica o estilo atualizado ao botão
-        console.log(tarefas);
+        tarefas[indice].concluida = permitido; //Atualiza o objeto no array com base no novo estado.
+        salvarTarefasNoStorage(); //Salva as alterações no localStorage.
+        aplicarEstiloBotaoConfirmar(link, permitido, 'COMPLETA', 'PENDENTE'); //Aplica o estilo atualizado ao botão.
     });
 
     return link;
@@ -103,39 +104,39 @@ const botaoConfirmar = (indice) => { //Cria um botão para confirmar a conclusã
 //----------------------------------------------------
 //#region Estilos de elementos HTML
 
-const aplicarEstiloMarcaDagua = (elemento) => {
-    const estilo = localStorage.getItem('estiloMarcaDagua') || 'nenhum';
+const aplicarEstiloMarcaDagua = (elemento) => { //Aplica qual o estilo que foi selecionado pelo usuário.
+    const estilo = localStorage.getItem('estiloMarcaDagua') || 'nenhum'; //Pega o item da "marca d'água".
 
-    elemento.classList.remove('marca-canto', 'marca-centro');
+    elemento.classList.remove('marca-canto', 'marca-centro'); //Remove os elementos da marca d'água.
 
-    if (estilo === 'canto') {
-        elemento.classList.add('marca-canto');
-    } else if (estilo === 'centro') {
-        elemento.classList.add('marca-centro');
+    if (estilo === 'canto') { //Verifica qual o estilo da marca d'água está na classe do elemento.
+        elemento.classList.add('marca-canto'); //Adiciona "marca-canto" caso o estilo seja "canto".
+    } else if (estilo === 'centro') { //Verifica qual o estilo da marca d'água está na classe do elemento.
+        elemento.classList.add('marca-centro'); //Adiciona "marca-centro" caso o estilo seja "centro".
     }
 };
 
-const definirCorDaPrioridade = (elemento, prioridade) => {
-    if (prioridade === 'Alta') {
-        elemento.style.backgroundColor = '#FF7F7F';
-    } else if (prioridade === 'Média') {
-        elemento.style.backgroundColor = '#ffb347'
-    } else {
-        elemento.style.backgroundColor = '#2ecc71';
+const definirCorDaPrioridade = (elemento, prioridade) => { //Define qual a cor de cada prioridade alta, media e baixa.
+    if (prioridade === 'Alta') { //Verifica qual a prioridade da tarefa.
+        elemento.style.backgroundColor = '#FF7F7F'; //Adiciona a cor Vermelha.
+    } else if (prioridade === 'Média') { //Verifica qual a prioridade da tarefa.
+        elemento.style.backgroundColor = '#ffb347' //Adiciona a cor Amarela.
+    } else { //Verifica qual a prioridade da tarefa.
+        elemento.style.backgroundColor = '#2ecc71'; //Adiciona a cor Verde.
     }
 }
 
 const aplicarEstiloBotaoConfirmar = (botao, permitido) => { //Aplica estilos ao botão de confirmar tarefa.
     botao.className = 'btn-confirmar'; // sempre aplica a classe base
 
-    if (permitido) {
-        botao.textContent = 'COMPLETA';
-        botao.classList.add('completa');
-        botao.classList.remove('pendente');
+    if (permitido) { //Altera o estilo do botão, para diferenciar das tarefas concluidas para as inconcluídas.
+        botao.textContent = 'COMPLETA'; //Coloca o texto do botão como "COMPLETA".
+        botao.classList.add('completa'); //Adiciona a classe "completa" ao botão.
+        botao.classList.remove('pendente'); //Remove a classe "pendente" ao botão.
     } else {
-        botao.textContent = 'PENDENTE';
-        botao.classList.add('pendente');
-        botao.classList.remove('completa');
+        botao.textContent = 'PENDENTE'; //Coloca o texto do botão como "PENDENTE".
+        botao.classList.add('pendente'); //Adiciona a classe "pendente" ao botão.
+        botao.classList.remove('completa'); //Remove a classe "completa" ao botão.
     }
 };
 //#endregion Estilos de elementos HTML
@@ -147,67 +148,69 @@ const aplicarEstiloBotaoConfirmar = (botao, permitido) => { //Aplica estilos ao 
 //#region {Funções Principais 
 
 //#region Adicionar Tarefa
-// Adiciona uma nova tarefa ao array de tarefas e atualiza o localStorage.
+//Decidindo o formato do array de armazenamento das informações de cada tarefa.
 const adicionarTarefa = (afazer, preferencia, tarefaStatus) => {
-    const tarefa = { //Cria um objeto tarefa com as informações fornecidas
+    const tarefa = { //Cria um objeto tarefa com as informações fornecidas.
         descricao: afazer,
         prioridade: preferencia,
         concluida: tarefaStatus,
-        data: dataAtual()
+        data: dataAtual() //Pega a data e hora atual.
     }
 
-    tarefas.push(tarefa); //Adiciona a nova tarefa ao array de tarefas
-    salvarTarefasNoStorage(); //Salva o array atualizado no localStorage
+    tarefas.push(tarefa); //Adiciona a nova tarefa ao array de tarefas.
+    salvarTarefasNoStorage(); //Salva o array atualizado no localStorage.
     return tarefa;
 };
 
-const adicionar = () => { //Função para adicionar uma nova tarefa à lista
+const adicionar = () => { //Função para adicionar uma nova tarefa à lista.
     const [inputTarefa, prioridadeValue] = informacoes();
 
-    if (inputTarefa.value === "") { //Verifica se o campo de tarefa está vazio
-        alert("Digite uma tarefa!");
+    if (inputTarefa.value === "") { //Verifica se o campo de tarefa está vazio.
+        alert("Digite uma tarefa!"); 
         return;
     }
 
-    adicionarTarefa(inputTarefa.value, prioridadeValue, false); // false = incompleta
+    adicionarTarefa(inputTarefa.value, prioridadeValue, false); //Vai pegar as informações e adicionar ao array.
 
-    exibirTarefaDoIndice(tarefas.length - 1); //Adiciona a tarefa ao array e exibe na lista
-    inputTarefa.value = ''; //Limpa o campo de entrada após adicionar a tarefa
-    console.log(tarefas);
+    exibirTarefaDoIndice(tarefas.length - 1); //Adiciona a tarefa ao array e exibe na lista.
+    inputTarefa.value = ''; //Limpa o campo de entrada após adicionar a tarefa.
+    console.log(tarefas); //Log das tarefas.
 };
 //#endregion Adicionar Tarefa
 //----------------------------------------------------
 //#region Exibição das Tarefas
-// Exibe as tarefas na lista HTML, criando elementos para cada tarefa e adicionando botões de ação.
+//Exibe as tarefas na lista HTML, criando elementos para cada tarefa e adicionando botões de ação.
 const exibirTarefaDoIndice = (indice) => {
     const { descricao, prioridade, data } = tarefas[indice]
-    // Coleta as informações necessárias para exibir a tarefa
+    //Coleta as informações necessárias para exibir a tarefa.
     const [, , listaTarefas] = informacoes();
 
-    const novaTarefa = document.createElement("li"); //Cria um novo elemento de lista para a tarefa
-    novaTarefa.className = 'text-list'; //Criando uma ClassName para a tag 'li'
-    novaTarefa.setAttribute('data-prioridade', `${prioridade} Prioridade`);
+    const novaTarefa = document.createElement("li"); //Cria um novo elemento de lista para a tarefa.
+    novaTarefa.className = 'text-list'; //Criando uma ClassName para a tag 'li'.
 
-    definirCorDaPrioridade(novaTarefa, prioridade); //Coloca a cor de fundo de acordo com cada prioridade
-    aplicarEstiloMarcaDagua(novaTarefa);
+    //Decidido o atributo que adicionará as prioridade.
+    novaTarefa.setAttribute('data-prioridade', `${prioridade} Prioridade`); 
 
-    const texto = document.createElement("span"); //Cria um elemento de texto para exibir a descrição da tarefa
-    texto.textContent = `${descricao} - Data: ${data}`; //Define o texto da tarefa com as informações coletadas
+    definirCorDaPrioridade(novaTarefa, prioridade); //Coloca a cor de fundo de acordo com cada prioridade.
+    aplicarEstiloMarcaDagua(novaTarefa); //Aplica o estilo da marca d'água, e onde será exibida.
 
-    const containerBotoes = document.createElement("div"); //Cria um contêiner para os botões de ação
-    containerBotoes.style.display = "flex"; //Define o display do contêiner como flexível
-    containerBotoes.style.gap = "8px"; //Adiciona um espaçamento entre os botões
+    const texto = document.createElement("span"); //Cria um elemento de texto para exibir a descrição da tarefa.
+    texto.textContent = `${descricao} - Data: ${data}`; //Define o texto da tarefa com as informações coletadas.
 
-    const removerTarefa = botaoRemover(indice); //Cria o botão de remover tarefa, passando o índice da tarefa
-    const confirmar = botaoConfirmar(indice); //Cria o botão de confirmar tarefa, passando o índice da tarefa
+    const containerBotoes = document.createElement("div"); //Cria um contêiner para os botões de ação.
+    containerBotoes.style.display = "flex"; //Define o display do contêiner como flexível.
+    containerBotoes.style.gap = "8px"; //Adiciona um espaçamento entre os botões.
 
-    containerBotoes.appendChild(confirmar); //Adiciona o botão de confirmar ao contêiner de botões
-    containerBotoes.appendChild(removerTarefa); //Adiciona o botão de remover ao contêiner de botões
+    const removerTarefa = botaoRemover(indice); //Cria o botão de remover tarefa, passando o índice da tarefa.
+    const confirmar = botaoConfirmar(indice); //Cria o botão de confirmar tarefa, passando o índice da tarefa.
 
-    novaTarefa.appendChild(texto); //Adiciona o texto da tarefa ao elemento de lista
-    novaTarefa.appendChild(containerBotoes); //Adiciona o contêiner de botões ao elemento de lista
+    containerBotoes.appendChild(confirmar); //Adiciona o botão de confirmar ao contêiner de botões.
+    containerBotoes.appendChild(removerTarefa); //Adiciona o botão de remover ao contêiner de botões.
 
-    listaTarefas.appendChild(novaTarefa); //Adiciona a nova tarefa à lista de tarefas no HTML
+    novaTarefa.appendChild(texto); //Adiciona o texto da tarefa ao elemento de lista.
+    novaTarefa.appendChild(containerBotoes); //Adiciona o contêiner de botões ao elemento de lista.
+
+    listaTarefas.appendChild(novaTarefa); //Adiciona a nova tarefa à lista de tarefas no HTML.
 };
 //#endregion Exibição das Tarefas
 
@@ -217,21 +220,20 @@ const exibirTarefaDoIndice = (indice) => {
 
 //#region {Limpeza de Tarefas
 
-const limparTarefas = () => { //Função para limpar todas as tarefas da lista
+const limparTarefas = () => { //Função para limpar todas as tarefas da lista.
     const [input, , lista] = informacoes();
 
-    if (tarefas.length === 0) { //Verifica se não há tarefas para limpar
+    if (tarefas.length === 0) { //Verifica se não há tarefas para limpar.
         alert("Não há tarefas para limpar!");
         return;
     }
-    //Solicita confirmação do usuário antes de limpar as tarefas
+    //Solicita confirmação do usuário antes de limpar as tarefas.
     const confirm = window.confirm("Você tem certeza que deseja limpar todas as tarefas?");
-    if (confirm) { //Se o usuário confirmar, limpa as tarefas
-        tarefas.length = 0; //Limpa o array de tarefas
-        salvarTarefasNoStorage(); //Atualiza o localStorage
-        lista.innerHTML = ''; //Limpa a lista visual
-        input.value = '';
-        console.log(tarefas);
+    if (confirm) { //Se o usuário confirmar, limpa as tarefas.
+        tarefas.length = 0; //Limpa o array de tarefas.
+        salvarTarefasNoStorage(); //Atualiza o localStorage.
+        lista.innerHTML = ''; //Limpa a lista visual.
+        input.value = ''; //Limpa o campo de entrada após adicionar a tarefa.
     }
 };
 //#endregion Limpeza de Tarefas}
@@ -239,11 +241,11 @@ const limparTarefas = () => { //Função para limpar todas as tarefas da lista
 //===============================================================
 
 //#region {Eventos
-window.onload = () => {
-    const estiloSalvo = localStorage.getItem('estiloMarcaDagua') || 'nenhum';
-    document.getElementById('estiloMarcaDagua').value = estiloSalvo;
+window.onload = () => { //Executará todas as informações guardadas ao carregar da página.
+    const estiloSalvo = localStorage.getItem('estiloMarcaDagua') || 'nenhum'; //Pega o estilo das marcas d'água.
+    document.getElementById('estiloMarcaDagua').value = estiloSalvo; //Define o valor do estilo das marcas d'água.
 
-    exibirTodasAsTarefas();
-    console.log(tarefas);
+    exibirTodasAsTarefas(); //Exibe todas as tarefas salvas.
+    console.log(tarefas); //Log das tarefas.
 };
 //#endregion Eventos}
